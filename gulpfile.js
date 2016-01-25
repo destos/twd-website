8e501cb349e3935f8cce6553749674c51d411eb7
Patrick Forringer <patrick@forringer.com>
2016-01-24 20:58:58 -0600

    Switch to gulp-sass
    switching due to inability to build with gulp-scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,36 @@
-/*global require*/
-(function (r) {
-  "use strict";
-  var scss = r("gulp-scss");
-  var gulp = r("gulp");
-  var livereload = require('gulp-livereload');
-  var webserver = require('gulp-webserver');
+'use strict'
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var livereload = require('gulp-livereload')
+var webserver = require('gulp-webserver')
 
-  gulp.task("scss", function () {
-    gulp.src(
-      "scss/main.scss"
-    ).pipe(scss(
-      {"bundleExec": false}
-    )).pipe(gulp.dest("./src/css"))
-    .pipe(livereload());
-  });
+gulp.task('scss', function () {
+  gulp.src('scss/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('src/css/'))
+  .pipe(livereload())
+})
 
-  gulp.task('webserver', function() {
-    gulp.src('src/')
-      .pipe(webserver({
-        open: true,
-        port: 8080
-      }));
-  });
+gulp.task('webserver', function () {
+  gulp.src('src/')
+    .pipe(webserver({
+      open: true,
+      port: 8080
+    }))
+})
 
-  gulp.task('html', function() {
-    return gulp.src([
-      'src/**/*.html'
-    ])
-    .pipe(livereload());
-  });
+gulp.task('html', function () {
+  return gulp.src([
+    'src/**/*.html'
+  ])
+  .pipe(livereload())
+})
 
-  gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('src/*.html', ['html']);
-    gulp.watch('scss/**/*.scss', ['scss']);
-  });
+gulp.task('watch', function () {
+  livereload.listen()
+  gulp.watch('src/*.html', ['html'])
+  gulp.watch('scss/**/*.scss', ['scss'])
+})
 
-  gulp.task('build', ['scss']);
-  gulp.task('default', ['webserver', 'watch']);
-}(require));
+gulp.task('build', ['scss'])
+gulp.task('default', ['webserver', 'watch'])
