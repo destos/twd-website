e88fec42ddbd247d415ba963a7f65e05361483cb
nearbycoder <nearbycoder@gmail.com>
2016-02-02 19:33:59 -0600

    Updated gulp file to move files to dist folder
    Gulp task for gulp build created to allow for building all files into
    the dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 var livereload = require('gulp-livereload')
 var webserver = require('gulp-webserver')
+var gulpCopy = require('gulp-copy')
+var clean = require('gulp-clean')
 
 gulp.task('scss', function () {
   gulp.src('scss/*.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('src/css/'))
+  .pipe(gulp.dest('./dist/css/'))
+  .pipe(gulp.dest('./src/css/'))
   .pipe(livereload())
 })
 
@@ -20,11 +23,22 @@ gulp.task('webserver', function () {
 })
 
 gulp.task('html', function () {
-  return gulp.src([
-    'src/**/*.html'
-  ])
+  return gulp.src('./src/**/*.html')
+  .pipe(gulpCopy('./dist', {prefix: 1}))
   .pipe(livereload())
 })
+
+gulp.task('assets', function () {
+  return gulp.src(['./src/img/*'])
+  .pipe(gulpCopy('./dist', {prefix: 1}))
+  .pipe(livereload())
+})
+
+gulp.task('clean', function () {
+	return gulp.src('./dist', {read: false})
+		.pipe(clean());
+})
+
 
 gulp.task('watch', function () {
   livereload.listen()
@@ -32,5 +46,5 @@ gulp.task('watch', function () {
   gulp.watch('scss/**/*.scss', ['scss'])
 })
 
-gulp.task('build', ['scss'])
+gulp.task('build', ['clean', 'scss', 'assets', 'html'])
 gulp.task('default', ['webserver', 'watch'])
